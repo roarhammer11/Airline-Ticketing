2d9c94047145c7c90caaabc64861a3858dd184b7
ash151515 <roarhammer11@gmail.com>
2021-07-04 22:10:29 +0800

    added components

--- AirplaneTicketingSystem.java
+++ AirplaneTicketingSystem.java
@@ -1,77 +1,84 @@
 import java.awt.*;
 
 import javax.swing.*;
+//import javax.swing.border.Border;
+
 
 public class AirplaneTicketingSystem 
 {
-
-	
-	
 	public AirplaneTicketingSystem()
-	{
-		JPanel panel = new JPanel();
-		JLabel chooseAirlineLabel = new JLabel("Choose Airline");
-		panel.add(chooseAirlineLabel);
+	{   
+		JPanel right = new JPanel();
+		right.setLayout(new BorderLayout());
+		JPanel left = new JPanel();
+		left.setLayout(new BorderLayout());
 		
-		String[] airlineChoices = {"Philippine Airline", "Air Asia", "Cebu Pacific", "Quatar Airways", "Saudia"};
-        JComboBox<String> airline = new JComboBox<String>(airlineChoices);
-        panel.add(airline);
-          
-        JLabel chooseTripTypeLabel = new JLabel("Choose Trip Type");
-        panel.add(chooseTripTypeLabel);
+		//JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, right, left);
+        
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(right, BorderLayout.EAST);
+        panel.add(left, BorderLayout.WEST);
+        
+    
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        left.add(leftPanel, BorderLayout.NORTH);
+        
+        String[] airlineChoices = {"Philippine Airline", "Air Asia", "Cebu Pacific", "Quatar Airways", "Saudia"};
+        addComponent("Choose Airline", leftPanel, airlineChoices);
         String[] tripTypeChoices = {"One-Way Trip", "Round Trip"};
-        JComboBox<String> tripType = new JComboBox<String>(tripTypeChoices);
-        panel.add(tripType);
-        
-        JLabel setScheduleLabel = new JLabel("Set Schedule");
-        panel.add(setScheduleLabel);
+        addComponent("Choose Trip Type", leftPanel, tripTypeChoices);
         String[] scheduleChoices = {"EMPTY"};
-        JComboBox<String> schedule = new JComboBox<String>(scheduleChoices);
-        panel.add(schedule);
-        
-        JPanel panel2 = new JPanel();
-        panel2.setAlignmentX(-1);
-        panel.add(panel2);
-        JLabel chooseFlightType = new JLabel("Choose Flight Type");
-        panel2.add(chooseFlightType);
+        addComponent("Set Schedule", leftPanel, scheduleChoices);
         String[] flightTypeChoices = {"International", "Local"};
-        JComboBox<String> flightType = new JComboBox<String>(flightTypeChoices);
-        panel2.add(flightType);
-        
-        JLabel setOrigin = new JLabel("Set Origin");
-        panel.add(setOrigin);
+        addComponent("Choose Flight Type", leftPanel, flightTypeChoices);
         String[] originChoices = {"EMPTY"};
-        JComboBox<String> origin = new JComboBox<String>(originChoices);
-        panel.add(origin);
-        
-        JLabel setDestination = new JLabel("Set Destination");
-        panel.add(setDestination);
+        addComponent("Set Origin", leftPanel, originChoices);
         String[] destinationChoices = {"EMPTY"};
-        JComboBox<String> destination = new JComboBox<String>(destinationChoices);
-        panel.add(destination);
-        
-        JLabel chooseClassType = new JLabel("Choose Class Type");
-        panel.add(chooseClassType);
+        addComponent("Set Destination", leftPanel, destinationChoices);
         String[] classTypeChoices = {"Business", "Economy", "Premium"};
-        JComboBox<String> classType = new JComboBox<String>(classTypeChoices);
-        panel.add(classType);
+        addComponent("Choose Class Type", leftPanel, classTypeChoices);
+        addComponent("Number Of Passengers", leftPanel, true);
+        String[] passengerType = {"Adult","Infant", "Senior Citizen"};
+        addComponent("Passenger Type", leftPanel, passengerType);
+        String[] modeOfPayment = {"Credit","Debit", "Cash"};
+        addComponent("Mode of Payment", leftPanel, modeOfPayment);
+        //TODO: Bank details, Change flight, Cancel flight, UI
         
- 
-        
-        
+       
 		JFrame frame = new JFrame();
 		frame.setTitle("Airplane Ticketing System");
 		frame.setLayout(new BorderLayout());
-        frame.add(panel, BorderLayout.WEST);
+        frame.add(panel, BorderLayout.CENTER);
         frame.setSize(new Dimension(800, 650));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
+        //frame.pack();
         frame.setVisible(true);
         
-        
-        
-        
-      
+	}
+	private void addComponent(String description, Container container, String[] list)
+	{
+		JLabel label = new JLabel(description);
+		container.add(label, BorderLayout.WEST);
+		JComboBox<String> box = new JComboBox<String>(list);
+		container.add(box, BorderLayout.WEST);
+	}
+	private void addComponent(String description, Container container, Boolean needsInput)
+	{
+		JLabel label = new JLabel(description);
+		container.add(label, BorderLayout.WEST);
+		
+		if (needsInput)
+		{
+			JTextArea infoTextArea = new JTextArea();
+	        infoTextArea.setLineWrap(true);
+	        infoTextArea.setWrapStyleWord(true);
+	        infoTextArea.setBackground(new Color(241,241,241));
+	        infoTextArea.setEditable(true);
+	        infoTextArea.setMargin(new Insets(5, 5, 5, 5));
+	        container.add(infoTextArea, BorderLayout.WEST);
+		}
 	}
 }
