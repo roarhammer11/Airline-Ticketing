437c144f3d2d3873d50ecf3f967af77f078ec25e
ash151515 <roarhammer11@gmail.com>
2021-07-05 01:24:03 +0800

    1

--- Run.java
+++ Run.java
@@ -1,10 +1,20 @@
+import javax.swing.SwingUtilities;
 
 public class Run {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		 new AirplaneTicketingSystem();
+		SwingUtilities.invokeLater(() ->
+		{
+			// TODO Auto-generated method stub
+			 new AirplaneTicketingSystem();
+		});
+//		SwingUtilities.invokeLater(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				new AirplaneTicketingSystem();
+//			}});
 	}
-
 }
